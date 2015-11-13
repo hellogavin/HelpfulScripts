ca6173da9f70e46c89cb7cad84cc73e283d366f6
hellogavin <1125692839@qq.com>
2015-11-13 17:03:46 +0800

    add missing script handle

---
+++ DeleteMissingScript.cs
@@ -0,0 +1,33 @@
+﻿/*******************************************
+*
+*  DeleteNullScript
+*
+*  Created by Gavin on 2015/11/13 16:51:28.
+*  
+*******************************************/
+
+using UnityEditor;
+using UnityEngine;
+
+public class DeleteMissingScript
+{
+    [MenuItem("Edit/DeleteMissingScripts &c")]
+    public static void DeleteNull()
+    {
+        foreach (var gameObject in Selection.gameObjects)
+        {
+            var components = gameObject.GetComponents<Component>();
+            var serializedObject = new SerializedObject(gameObject);
+            var prop = serializedObject.FindProperty("m_Component");
+            var r = 0;
+            for (var j = 0; j < components.Length; j++)
+            {
+                if (components[j] != null) continue;
+                prop.DeleteArrayElementAtIndex(j - r);
+                r++;
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+        AssetDatabase.Refresh();
+    }
+}
